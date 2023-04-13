9615a366f32ba4d71da721eb3d3fd05e59327143
ArdeshirV <e-job@protonmail.com>
2023-04-13 07:01:58 +0330

    Add ReadLine() function

---
+++ ReadLine.go
@@ -0,0 +1,13 @@
+func ReadLine() string  {
+  inputLine := ""
+  for {
+    var character byte
+    inputCount, err := fmt.Scanf("%c", &character)  // Read just one character
+    if err != nil { panic(err) }  // If there is IO error then panic
+    if inputCount <= 0 { break }  // Exit if there is not any new input character
+    // Guarantee it Works on Linux, Windows and Mac
+    if character == '\n' || character == '\r' { break }  // Reach to end of the line
+    inputLine += string(character)
+  }
+  return inputLine
+}
